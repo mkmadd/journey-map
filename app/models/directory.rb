6b65794576293a86ad68a363b36cb54f7a681d2c
Michael K. Maddeford <mkmadd@gmail.com>
2014-06-10 19:22:22 +0000

    integrate dir_hash from dir_reader.rb

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,11 +1,12 @@
 class Directory
+  require_relative '../hacks/dir_reader' 
 
   def root
     root = '/journey-map'
   end
   
   def root_contents
-    root_contents = Dir.entries('.')
+    root_contents = dir_hash('.') #Dir.entries('.')
   end
 
 end
